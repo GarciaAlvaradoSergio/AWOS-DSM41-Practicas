fb32cc6d8937d43d2986d168186e35ebe52cd5e6
Sergio Garcia <al222110820@gmail.com>
2022-11-05 23:03:22 -0600

    layout

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -23,36 +23,43 @@
 
         <a href="/">
             <div class="option">
-                <i class="far fa-file" title="Portafolio"></i>
+                <i class="far fa-id-badge" title="Usuarios"></i>
                 <h4>Usuarios</h4>
             </div>
         </a>
         
         <a href="/">
             <div class="option">
-                <i class="fas fa-video" title="Cursos"></i>
-                <h4>Cursos</h4>
+                <i class="fas fa-video" title="Eventos"></i>
+                <h4>Eventos</h4>
             </div>
         </a>
 
         <a href="/">
             <div class="option">
-                <i class="far fa-sticky-note" title="Blog"></i>
-                <h4>Blog</h4>
+                <i class="far fa-sticky-note" title="Actividades"></i>
+                <h4>Actividades</h4>
             </div>
         </a>
 
         <a href="/">
             <div class="option">
-                <i class="far fa-id-badge" title="Contacto"></i>
-                <h4>Contacto</h4>
+                <i class="far fa-file" title="Subactividades"></i>
+                <h4>Subactividades</h4>
             </div>
         </a>
 
         <a href="/">
             <div class="option">
-                <i class="far fa-address-card" title="Nosotros"></i>
-                <h4>Nosotros</h4>
+                <i class="far fa-address-card" title="Categorias"></i>
+                <h4>Categorias</h4>
+            </div>
+        </a>
+
+        <a href="/">
+            <div class="option">
+                <i class="far fa-address-card" title="Estado"></i>
+                <h4>Estado</h4>
             </div>
         </a>
 
