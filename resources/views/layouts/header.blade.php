5a90c7ea70d5356a10a184cc2e605a4dbfb3a98d
Sergio Garcia <al222110820@gmail.com>
2022-11-12 01:58:31 -0600

    layouts

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,7 @@
     <title>Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -22,7 +22,7 @@
     <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
 
     <!-- Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="{{asset('vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 
 </head>
 <style>
@@ -40,6 +40,12 @@
         background-repeat: no-repeat;
         text-align: center;
         padding: .1px;
+    }
+    .back{
+        background-color: #929EAD;
+        width: 20px;
+        height: 20px;
+
     }
     .card__person{
         background-image: url('/scr/profile.svg');
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,17 +9,20 @@
     <div class="container-fluid">
             <!-- Page Heading -->
         <div class="alert alert-secondary" role="alert">
-        Inicio/Tablas/Estudiantes/<a href="#" class="alert-link">{{ $item->name }}</a>.
+        Inicio/Tablas/<a href="/users" class="alert-link">Estudiantes/ </a>{{ $item->name }}.
         </div>
     </div>
 
         <section class="tarjeta">
             <div class="card__person">
             </div>
-                <h5 class="card__name">{{$item->name}}</h5>
-                <p class="card__position">Estudiante</p>
-                <p class="card__position">Correo:</p>
-                <p class="card__position">{{$item->email}}</p>
+            <h5 class="card__name">{{$item->name}}</h5>
+            <p class="card__position">Estudiante</p>
+            <p class="card__position">Correo:</p>
+            <p class="card__position">{{$item->email}}</p>
+            <div class="button-container">
+                <a href="{{ route('users.index') }}" class="btn btn-sm btn-success mr-0"> Volver </a>
+            </div>
         </section>
 
 </div>
