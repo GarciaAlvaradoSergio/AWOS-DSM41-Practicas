aab643f23ea118c77b6b7f7aff0876aea9f84f0f
Sergio Garcia <al222110820@gmail.com>
2022-12-06 23:06:32 -0600

    Factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            'nombreCategoria'=> $this->faker->randomElement(['Tareas',
+            'nombreCategoria'=> $this->faker->unique()->randomElement(['Tareas',
                                                             'Investigación',
                                                             'Importante',
                                                             'Documentación',
