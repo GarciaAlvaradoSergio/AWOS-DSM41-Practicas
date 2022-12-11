3cfda2e5b98b89699caae4e716f08bf08a993b4d
Sergio Garcia <al222110820@gmail.com>
2022-12-11 17:59:53 -0600

    Updates

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,18 +11,18 @@
 
                             <!-- Page Heading -->
                             <div class="alert alert-secondary" role="alert">
-                                Inicio/Tablas/<a href="#" class="alert-link">Categorias</a>.
+                                Inicio/Tablas/<a href="#" class="alert-link">Estudiantes</a>.
                               </div>
                             <!-- DataTales Example -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Tabla de datos de las categorias</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Tabla de datos de los estudiantes</h6>
                                 </div>
                                 <div class="card-body">
                                     <div class="table-responsive">
                                         <div class="card mb-3 border-left-primary" style="width: 5px 15px;">
                                             <div class="card-body" style="display: inline-flex;">
-                                                <p style="margin: auto 20px;">Agregar categoria</p><a href="users/create"
+                                                <p style="margin: auto 20px;">Agregar estudiantes</p><a href="users/create"
                                                 type="button" class="btn btn-primary">Agregar</a>
                                             </div>
                                         </div>
