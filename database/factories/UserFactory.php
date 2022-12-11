b94b58eac3d68037d9f99d8a3f1c584089381e66
Sergio Garcia <al222110820@gmail.com>
2022-12-10 21:15:32 -0600

    updates

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,11 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'nombre' => $this->faker->firstNameMale(),
-            'apellido' => $this->faker->lastName(),
-            'usuario' => $this->faker->userName(),
-            'correo' => $this->faker->email(),
-            'contraseña' => $this->faker->password(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.', // password
+            'remember_token' => Str::random(10),
         ];
     }
 }
--- database/migrations/2022_11_12_154224_create_users_table.php
+++ database/migrations/2022_11_12_154224_create_users_table.php
@@ -15,14 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre');
-            $table->string('apellido');
-            $table->string('usuario');
-            $table->string('correo');
-            $table->string('contraseña');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
-            $table->SoftDeletes();
-
         });
     }
 
